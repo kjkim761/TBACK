971095f666057237574c30e3c4b4c9286b2bd6ee
Kyoungjihn Kim <kjkim761@gmail.com>
2025-10-03 23:48:51 +0900

    onlytext

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{TimeDelta, Utc};
 use dioxus::{html::h1, prelude::*};
+use rand::Rng;
 
 fn main() {
     dioxus::launch(App);
@@ -6,13 +8,16 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    let mut location = use_signal(|| fastrand::i32(1..=3));
+    let mut max_interval = use_signal(||TimeDelta::zero());
+    let mut location = use_signal(|| rand::rng().random_range(1..=3));
     let mut phase = use_signal(|| 0);
     let mut countdown = use_signal(|| 0);
     let mut memo = use_signal(|| Vec::<i32>::with_capacity(30));
-    let mut now = use_signal(||std::time::SystemTime::now());
+    let mut now = use_signal(||Utc::now());
     rsx! {
         h1 { "TBACK" }
+        a { href: "https://github.com/kjkim761/TBACK", "<about>" }
+
         if phase() != 1 {
             div { id: "circles",
                 h1 {
@@ -35,19 +40,19 @@ fn App() -> Element {
             if location() == 1 {
                 div {
                     h1 { "③" }
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 {color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 2 {
                 div {
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "③" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 3 {
                 div {
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "③" }
                 }
             }
@@ -55,19 +60,19 @@ fn App() -> Element {
             if location() == 1 {
                 div {
                     h1 { "②" }
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 2 {
                 div {
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "②" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 3 {
                 div {
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "②" }
                 }
             }
@@ -75,19 +80,19 @@ fn App() -> Element {
             if location() == 1 {
                 div {
                     h1 { "①" }
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 2 {
                 div {
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "①" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
                 }
             } else if location() == 3 {
                 div {
-                    h1 { "◯" }
-                    h1 { "◯" }
+                    h1 { color:"white", "◯" }
+                    h1 { color:"white", "◯" }
                     h1 { "①" }
                 }
             }
@@ -97,26 +102,69 @@ fn App() -> Element {
                 onclick: move |ev| async move {
                     phase.set(1);
                     countdown.set(3);
-                    location.set(fastrand::i32(1..=3));
+                    location.set(rand::rng().random_range(1..=3));
                     memo.with_mut(|m| m.push(location()));
                     async_std::task::sleep(std::time::Duration::from_secs(1)).await;
                     countdown.set(2);
-                    location.set(fastrand::i32(1..=3));
+                    location.set(rand::rng().random_range(1..=3));
                     memo.with_mut(|m| m.push(location()));
                     async_std::task::sleep(std::time::Duration::from_secs(1)).await;
                     countdown.set(1);
-                    location.set(fastrand::i32(1..=3));
+                    location.set(rand::rng().random_range(1..=3));
                     memo.with_mut(|m| m.push(location()));
                     async_std::task::sleep(std::time::Duration::from_secs(1)).await;
                     phase.set(2);
-                    location.set(fastrand::i32(1..=3));
+                    location.set(rand::rng().random_range(1..=3));
                     memo.with_mut(|m| m.push(location()));
+                    now.set(Utc::now());
                 },
                 "Start"
             }
         } else if phase() == 2 {
-            button { "Match" }
-            button { "Unmatch" }
+            button {
+                onclick: move |ev| {
+                    if *memo().last().unwrap() == memo()[memo().len() - 4] {
+                        let interval = Utc::now() - now();
+                        now.set(Utc::now());
+                        if interval > max_interval() {
+                            max_interval.set(interval);
+                        }
+                        if memo().len() >= 23 {
+                            phase.set(4)
+                        }
+                        location.set(rand::rng().random_range(1..=3));
+                        memo.with_mut(|m| m.push(location()));
+                    } else {
+                        phase.set(3);
+                    }
+                },
+                "Match"
+            }
+            button {
+                onclick: move |ev| {
+                    if *memo().last().unwrap() != memo()[memo().len() - 4] {
+                        let interval = Utc::now() - now();
+                        now.set(Utc::now());
+                        if interval > max_interval() {
+                            max_interval.set(interval);
+                        }
+                        if memo().len() >= 23 {
+                            phase.set(4)
+                        }
+                        location.set(rand::rng().random_range(1..=3));
+                        memo.with_mut(|m| m.push(location()));
+                    } else {
+                        phase.set(3);
+                    }
+                },
+                "Unmatch"
+            }
+        } else if phase() == 3 {
+            h1 { "Wrong!" }
+            a { href: "/", "Restart" }
+        } else if phase() == 4 {
+            h1 { "Clear! Your score is {max_interval}." }
+            a { href: "/", "Restart" }
         }
     }
 }
